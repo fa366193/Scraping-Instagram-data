cde9223c4a84ca3effcbfb5d0cf2df41ee6e34c0
Fatima Aguilar <123963635+fa366193@users.noreply.github.com>
2023-02-24 10:32:38 -0500

    Add files via upload

---
+++ Scraping instagram data.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import the module
+get_ipython().system('pip install instaloader')
+import instaloader
+
+# Create an instance of Instaloader class
+bot = instaloader.Instaloader()
+
+# Load a profile from an Instagram handle
+profile = instaloader.Profile.from_username(bot.context, 'books.before.thoughts')
+
+print(type(profile))
+
+
+# In[2]:
+
+
+print("Username: ", profile.username)
+print("User ID: ", profile.userid)
+print("Number of Posts: ", profile.mediacount)
+print("Followers: ", profile.followers)
+print("Followees: ", profile.followees)
+print("Bio: ", profile.biography,profile.external_url)
+
+
+# In[3]:
+
+
+# Load a new profile
+profile = instaloader.Profile.from_username(bot.context, 'wwe')
+
+# Get all posts in a generator object
+posts = profile.get_posts()
+
+# Iterate and download
+for index, post in enumerate(posts, 1):
+    bot.download_post(post, target=f"{profile.username}_{index}")
+
+
+# In[ ]:
+
+
+
+
